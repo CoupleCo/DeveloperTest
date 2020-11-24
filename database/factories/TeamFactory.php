8251339ea7945efa8462e64006b5946c5c5994f5
Dalhatu Njidda <dalsdnjidda@gmail.com>
2020-11-24 10:57:33 +0100

    add models, controllers, migrations and factories

---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Team;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Team::class, function (Faker $faker) {
+    return [
+    	'user_id' => function() {
+    		return factory('App\User')->create()->id;
+    	},
+        'name' => $faker->text(30),
+        'description' => $faker->paragraph
+    ];
+});
